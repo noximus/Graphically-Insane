4b13132a6b0cf8c0501183175e81a245fc2cf310
Henry Richardson <hrichardson83@gmail.com>
2020-04-14 17:29:31 -0400

    removed all previous code

--- app/assets/js/config.js
+++
@@ -1,9 +0,0 @@
-(function () {
-'use strict';
- angular.module('config', [])
-
-.constant('envPackage', {version:'1.0.0',env:'DEV',description:'We are in development!'})
-
-.value('debug', false)
-
-; })();
