6b288b1beb5ef947fb4988d7dc0de1e2535048e7
Henry Richardson <hrichardson83@gmail.com>
2020-04-15 15:49:31 -0400

    adding some menu logic

--- src/App.js
+++ src/App.js
@@ -7,46 +7,63 @@ import { TweenMax, Bounce } from 'gsap';
 
 function App() {
   let menuIconAni = useRef(null);
+  let pageMenu = useRef(null);
   useEffect(() => {
     TweenMax.from(menuIconAni, 2, {
       y: -20,
       ease: Bounce.easeOut,
     });
   }, []);
-  // console.log(menuIcon);
+
+  function openMenu(e) {
+    TweenMax.to(pageMenu, 3, {
+      left: '80%',
+      scale: 0.9,
+      ease: Bounce.easeOut,
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="btn-menu">
-          <img
-            ref={(el) => {
-              menuIconAni = el;
-            }}
-            src={MenuIcon}
-            alt="menu"
-          />
+      <div
+        className="page"
+        ref={(el) => {
+          pageMenu = el;
+        }}
+      >
+        <header className="App-header">
+          <div className="btn-menu">
+            <button onClick={openMenu}>
+              <img
+                ref={(el) => {
+                  menuIconAni = el;
+                }}
+                src={MenuIcon}
+                alt="menu"
+              />
+            </button>
+          </div>
+          <div className="brand">Graphically Insane</div>
+          <div className="btn-settings">
+            <img src={SettingsIcon} alt="settings" />
+          </div>
+        </header>
+        <div className="body">
+          <div className="jumbotron">
+            <div className="main_img"></div>
+            <div className="second_img"></div>
+            <div className="third_img"></div>
+          </div>
         </div>
-        <div className="brand">Graphically Insane</div>
-        <div className="btn-settings">
-          <img src={SettingsIcon} alt="settings" />
-        </div>
-      </header>
-      <div className="body">
-        <div className="jumbotron">
-          <div className="main_img"></div>
-          <div className="second_img"></div>
-          <div className="third_img"></div>
-        </div>
+        <footer>
+          <div className="footer-nav">
+            <div className="icon icon-1">1</div>
+            <div className="icon icon-1">2</div>
+            <div className="icon icon-1">3</div>
+            <div className="icon icon-1">4</div>
+            <div className="icon icon-1">5</div>
+          </div>
+        </footer>
       </div>
-      <footer>
-        <div className="footer-nav">
-          <div className="icon icon-1">1</div>
-          <div className="icon icon-1">2</div>
-          <div className="icon icon-1">3</div>
-          <div className="icon icon-1">4</div>
-          <div className="icon icon-1">5</div>
-        </div>
-      </footer>
     </div>
   );
 }
